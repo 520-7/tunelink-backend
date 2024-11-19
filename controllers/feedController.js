1b6bb00967f4777ba70449b6c37b2f468d49384d
Ozel Yilmazel <ozelyilmazel@vl965-172-31-249-101.wireless.umass.edu>
2024-11-19 13:20:21 -0500

    recommendation done

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -58,18 +58,80 @@ async function getRandomPost(collection, query) {
 
 export const getFeed = async (req, res) => {
   try {
+    const amount = 50;
+
     const { userId } = req.params;
     const client = await getMongoClient();
     const db = client.db("app_data");
+    const usersCollection = db.collection("users");
     const postsCollection = db.collection("posts");
 
-    const posts = await postsCollection
+    const objId = ObjectId.createFromHexString(userId);
+
+    const user = await usersCollection.findOne({ _id: objId });
+    const following = user.following.map((id) =>
+      ObjectId.createFromHexString(id)
+    );
+
+    // 70% of returned posts are from followed users
+    // amount * 0.7 = 35
+    // if following no one, or not enough total posts, compensate with the posts from different genres and not followed
+    let fraction = 0.7;
+    let leftOver = amount;
+
+    let targetCount = Math.floor(amount * fraction);
+    let case1Posts = [];
+    if (following.length !== 0) {
+      case1Posts = await postsCollection
+        .find({ ownerUser: { $in: following } })
+        .sort({ timestamp: -1 })
+        .limit(targetCount)
+        .toArray();
+    }
+
+    leftOver -= case1Posts.length;
+
+    // 20% of returned posts has same genre
+    // amount * 0.2 = 10
+    // if not enough total posts, compensate with the posts from different genres and not followed
+
+    fraction = 0.2;
+    targetCount = Math.floor(amount * fraction);
+    let case2Posts = [];
+    if (user.genres.length !== 0) {
+      const usersWithMatchingGenres = await usersCollection
+        .find({
+          genres: { $in: user.genres },
+        })
+        .project({ _id: 1 })
+        .toArray();
+
+      const matchingUserIds = usersWithMatchingGenres.map((user) => user._id);
+
+      if (matchingUserIds.length !== 0) {
+        case2Posts = await postsCollection
+          .find({ ownerUser: { $in: matchingUserIds } })
+          .toArray();
+
+        case2Posts.sort(() => Math.random() - 0.5);
+        case2Posts = case2Posts.slice(0, targetCount);
+      }
+    }
+
+    leftOver -= case2Posts.length;
+
+    // rest of returned posts are random, most recent
+    targetCount = leftOver;
+    let case3Posts = await postsCollection
       .find()
-      .sort({ timestamp: -1 }) // Sort by 'timestamp' field in descending order to get the most recent posts first
-      .limit(50) // Limit to 50 posts
+      .sort({ timestamp: -1 })
+      .limit(targetCount)
       .toArray();
 
-    res.json({ feed: posts });
+    const recommendedPosts = [...case1Posts, ...case2Posts, ...case3Posts];
+    recommendedPosts.sort(() => Math.random() - 0.5);
+
+    res.json({ feed: recommendedPosts });
   } catch (error) {
     console.error("Error in /get_feed:", error);
     res.status(500).json({ error: "Internal server error." });
